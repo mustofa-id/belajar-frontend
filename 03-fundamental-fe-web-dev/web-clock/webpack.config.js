4f91929f32be6b439508101bc2348292b961efc6
mustofa-id <hm.dev7@gmail.com>
2022-09-18 13:35:12 +0700

    chore: webpace loader `babel-loader`

--- 03-fundamental-fe-web-dev/web-clock/webpack.config.js
+++ 03-fundamental-fe-web-dev/web-clock/webpack.config.js
@@ -13,6 +13,18 @@ const config = {
 			{
 				test: /\.css$/,
 				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+			},
+			{
+				test: /\.js/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env']
+						}
+					}
+				]
 			}
 		]
 	}
