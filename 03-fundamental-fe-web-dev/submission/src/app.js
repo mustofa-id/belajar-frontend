914ef911e86a5db303088f15ef1e6f53afd4de9f
mustofa-id <hm.dev7@gmail.com>
2022-10-10 09:54:05 +0700

    rethingking: change project to planing app

--- 03-fundamental-fe-web-dev/submission/src/app.js
+++ 03-fundamental-fe-web-dev/submission/src/app.js
@@ -2,7 +2,7 @@ import { user } from './data/user.js';
 
 const init = () => {
 	const h1 = document.createElement('h1');
-	h1.innerText = 'Hello, Amber Movie app!';
+	h1.innerText = 'Hello, plan app!';
 	const pre = document.createElement('pre');
 	pre.innerText = JSON.stringify(user, null, 2);
 	document.documentElement.append(h1, pre);
