25030b2f7a78cfd7017e591584c2af8c60cadfe2
mustofa-id <hm.dev7@gmail.com>
2022-12-18 17:06:24 +0700

    chore: initial design

--- 04-fe-expert/restaurant-apps/src/scripts/index.js
+++ 04-fe-expert/restaurant-apps/src/scripts/index.js
@@ -1,4 +1,43 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.scss';
+import data from '../DATA.json';
+import '../styles/index.scss';
 
-console.log('Hello Coders! :)');
+function setupTopBar() {
+	let timer;
+
+	const topBar = document.querySelector('.top-bar');
+	const hero = document.querySelector('.hero');
+
+	document.onscroll = () => {
+		if (timer) clearTimeout(timer);
+		timer = setTimeout(() => {
+			const rect = hero.getBoundingClientRect();
+			if (rect.bottom > 0) {
+				topBar.removeAttribute('data-floating');
+			} else {
+				topBar.setAttribute('data-floating', '');
+			}
+		}, 150);
+	};
+}
+
+function loadRestaurants() {
+	const restaurantContainer = document.querySelector('.restaurants');
+	for (const r of data.restaurants) {
+		const card = document.createElement('article');
+		card.innerHTML = `
+			<img src="${r.pictureId}" alt="Image of ${r.name} at ${r.city} city" loading="lazy"/>
+			<h2>${r.name}</h2>
+			<h3>${r.city} &bullet; ${r.rating} &star;</h2>
+			<p>${r.description}</p>
+		`;
+		restaurantContainer.appendChild(card);
+	}
+}
+
+function init() {
+	setupTopBar();
+	loadRestaurants();
+}
+
+document.addEventListener('DOMContentLoaded', init);
